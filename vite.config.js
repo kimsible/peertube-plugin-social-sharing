d0fd66a56b159ae48bb5d02f18508d6bf4774475
Ms Kimsible <kimsible@users.noreply.github.com>
2021-09-05 02:47:14 +0200

    Add livereload for development mode

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { resolve } from 'path'
 import { loadEnv } from 'vite'
 import pkg from './package.json'
+import livereload from 'rollup-plugin-livereload'
 
 // npx vite build -m development - build and watch without minifying in PeerTube environment
 // npx vite build -m staging - build without minifying
 // npx vite build [-m production] - build and minify
 
 export default async ({ mode }) => {
-  const buildPeerTubeDistPath = () => {
-    const env = loadEnv(mode, process.cwd(), 'PEERTUBE_')
-    return env.PEERTUBE_PATH && resolve(env.PEERTUBE_PATH, `./storage/plugins/node_modules/${pkg.name}/`)
-  }
+  const env = loadEnv(mode, process.cwd(), 'PEERTUBE_')
+  const buildPeerTubeDistPath = () => env.PEERTUBE_PATH && resolve(env.PEERTUBE_PATH, `./storage/plugins/node_modules/${pkg.name}/`)
 
   return {
     build: {
-      outDir: resolve((mode === 'development' && buildPeerTubeDistPath()) || './', 'dist'),
+      outDir: resolve(buildPeerTubeDistPath() || './', 'dist'),
       watch: mode === 'development',
       minify: mode === 'production' && 'terser',
       lib: {
@@ -23,6 +22,7 @@ export default async ({ mode }) => {
         formats: ['es'],
         fileName: () => 'common-client-plugin.js'
       }
-    }
+    },
+    plugins: [mode === 'development' && livereload()]
   }
 }
